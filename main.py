fba33acbd0ed75dcf9600d9ffe6788a69a7636dd
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-07-12 14:00:32 +0200

    updating

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from git.repo import Repo
-repository = Repo.init('/var/python_projects/test_folder.git', bare =True)
-f = open("/var/python_projects/test_folder.gitmyfiles.txt", "w")
+repository = Repo.init('test_folder.git', bare =True)
+f = open("test_folder.git/myfiles.txt", "w")
 f.close()
 print(repository.untracked_files)
