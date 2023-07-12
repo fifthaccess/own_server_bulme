205a17d93c092782bae830b24820d785efc07cbb
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-07-12 13:55:53 +0200

    updating main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from git.repo import Repo
 repository = Repo.init('/var/python_projects/test_folder.git', bare =True)
-f = open("/var/python_projects/test_folder.git/myfiles", "x")
+f = open("myfiles", "x")
 f.close()
