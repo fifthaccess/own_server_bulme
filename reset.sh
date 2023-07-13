a96e0d1a7ec01e6ea2e9465da05c2fc33527c335
fifthaccess <benedikt.erkner-sach@bulme.at>
2023-07-13 13:42:16 +0200

    fixed

--- reset.sh
+++ reset.sh
@@ -1,3 +1,3 @@
 sudo rm -r /var/python_projects/test/test.git
 mkdir /var/python_projects/test/test.git
-cp cp /var/python_projects/own_server_bulme/initgit.sh  /var/python_projects/test/test.git
+cp /var/python_projects/own_server_bulme/initgit.sh  /var/python_projects/test/test.git
